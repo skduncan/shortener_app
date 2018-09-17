bdfaaf0dbb9abf8adc35b43f656de8cafb6d6d99
Sam <samkduncan@gmail.com>
2018-09-17 23:50:01 +0000

    Finished creating Shortlink Create Page

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -1,4 +1,26 @@
 class ShortlinksController < ApplicationController
   def new
+    @shortlink = Shortlink.new
   end
+  
+  def show
+    @shortlink = Shortlink.find(params[:id])
+    #debugger
+  end
+  
+  def create
+    @shortlink = Shortlink.new(shortlink_params)
+    
+    if @shortlink.save
+      flash[:success] = "New Short Link has been generated"
+      redirect_to @shortlink
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def shortlink_params
+      params.require(:shortlink).permit(:shorturl, :longurl)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'shortener#home'
 
   get '/newlink', to: 'shortlinks#new'
+  post '/newlink', to: 'shortlinks#create'
+  
+  resources :shortlinks
   
 end
