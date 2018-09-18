7e1fbbea9f4e803899a882bebcd0677577c4784a
Sam <samkduncan@gmail.com>
2018-09-18 01:51:06 +0000

    Add table view for index page

--- app/models/shortlink.rb
+++ app/models/shortlink.rb
@@ -6,8 +6,8 @@ class Shortlink < ApplicationRecord
   validates :longurl, presence: true
   validates_format_of :longurl, :with => URI::regexp(%w(http https))
   
-  def format_url
-    self.longurl = “http://#{self.longurl}” unless self.website[/^https?/]
-  end
+ # def format_url
+  #  self.longurl = “http://#{self.longurl}” unless self.website[/^https?/]
+ # end
 
 end
