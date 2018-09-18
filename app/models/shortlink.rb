ba94510ef80e719225c817edfd4811f735ef4ea0
Sam <samkduncan@gmail.com>
2018-09-18 01:42:35 +0000

    Fix shortlinks redirect

--- app/models/shortlink.rb
+++ app/models/shortlink.rb
@@ -1,4 +1,13 @@
 class Shortlink < ApplicationRecord
+  
+  before_save :format_url
+
   validates :shorturl, presence: true
   validates :longurl, presence: true
+  validates_format_of :longurl, :with => URI::regexp(%w(http https))
+  
+  def format_url
+    self.longurl = “http://#{self.longurl}” unless self.website[/^https?/]
+  end
+
 end
