9c105cdf034591ecd41d53911465c83dc75b8a73
Sam <samkduncan@gmail.com>
2018-09-20 05:59:14 +0000

    Improved shorturl/longurl validations and added method for removing trailing slash at end of shorturl

--- app/models/shortlink.rb
+++ app/models/shortlink.rb
@@ -1,12 +1,12 @@
 class Shortlink < ApplicationRecord
   
-  before_save :format_url
+  before_save :format_url, :remove_slash
 
   validates :shorturl, presence: true
-  validates_format_of :shorturl, :with => /\A[a-zA-Z0-9_-]{2,30}\Z/
+  validates_format_of :shorturl, :with => /\A[a-zA-Z0-9\/:._-]{2,30}\Z/
   validates :longurl, presence: true
-  validates_format_of :longurl, :with => /\A[a-zA-Z0-9._-]{2,30}\Z/
- # validates_format_of :longurl, :with => URI::regexp(%w(http https)) # add checks for .com, .net, etc., filter out symbols (@, &, etc.)
+  validates_format_of :longurl, :with => /\A[a-zA-Z0-9\/:._-]{2,99}\Z/
+ # validates_format_of :longurl, :with => URI::regexp(%w(http https))
   
   def format_url
     if self.longurl.include?("https://") || self.longurl.include?("http://")
@@ -15,5 +15,10 @@ class Shortlink < ApplicationRecord
       self.longurl = "http://#{self.longurl}"
     end
   end
-
+  
+  def remove_slash
+    if self.shorturl.to_s.chars.last == '/'
+      self.shorturl = self.shorturl.chomp('/')
+    end
+  end
 end
