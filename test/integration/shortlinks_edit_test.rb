3480699630e99a07c7b0a2907c04c8305c125e9b
Sam <samkduncan@gmail.com>
2018-09-24 23:20:03 +0000

    Edited tests

--- test/integration/shortlinks_edit_test.rb
+++ test/integration/shortlinks_edit_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ShortlinksEditTest < ActionDispatch::IntegrationTest
   def setup
-    @shortlink = Shortlink.new(shorturl: "example", longurl: "www.google.com")
+    @shortlink = shortlinks(:google)
   end
   
   test "unsuccessful edit" do
@@ -12,21 +12,21 @@ class ShortlinksEditTest < ActionDispatch::IntegrationTest
                                               longurl: "g@@gle>com" }}
 
     assert_template 'shortlinks/edit'    
-    assert_select 'div.alert.alert-danger', text: "The form contains 1 error."
+    assert_select 'div.alert.alert-danger', text: "The form contains 3 errors."
   end
   
   test "successful edit with friendly forwarding" do
     get edit_shortlink_path(@shortlink)
-    assert_redirected_to edit_shortlink_url(@shortlink)
+    assert_template 'shortlinks/edit'
     shorturl_new  = "Example2"
-    longurl_new = "foobar.com"
+    longurl_new = "http://foobar.com"
     patch shortlink_path(@shortlink), params: { shortlink: { shorturl:  shorturl_new,
                                               longurl: longurl_new} }
     assert_not flash.empty?
-    assert_redirected_to @shortlink
+    assert_redirected_to root_path
     @shortlink.reload
-    assert_equal shorturl,  @shortlink.shorturl
-    assert_equal longurl, @shortlink.longurl
+    assert_equal shorturl_new,  @shortlink.shorturl
+    assert_equal longurl_new, @shortlink.longurl
     assert_redirected_to root_path
   end
   
